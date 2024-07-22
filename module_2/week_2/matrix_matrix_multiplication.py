305eeaf6fc5223530cd51267cbb08760491f9139
tom1209-netizen <lephuckhang0@gmail.com>
2024-07-22 23:19:46 +0700

    Finish module 2 week 2 HW

---
+++ module_2/week_2/matrix_matrix_multiplication.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def matrix_multi_matrix(matrix1, matrix2):
+    return np.dot(matrix1, matrix2)
---
+++ module_2/week_2/vector_length.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def compute_vector_length(vector):
+    return np.sqrt(np.sum(vector ** 2))
