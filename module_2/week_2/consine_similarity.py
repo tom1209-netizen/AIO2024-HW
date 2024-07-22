7dfb6d85649cadda9f26da2a8ac0f8b94d08c9ae
tom1209-netizen <lephuckhang0@gmail.com>
2024-07-22 23:19:46 +0700

    Finish module 2 week 2 HW

---
+++ module_2/week_2/consine_similarity.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def cosine_similarity(vector1, vector2):
+    return np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
---
+++ module_2/week_2/vector_dot_product.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def compute_dot_prodcut(vector1, vector2):
+    return np.dot(vector1, vector2)
